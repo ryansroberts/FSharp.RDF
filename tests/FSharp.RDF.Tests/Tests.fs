9d915abcbbbf833df921e362669e1b3ae97d8b5c
Ryan Roberts <ryansroberts@gmail.com>
2015-03-05 09:57:54 +0000

    Tests, that wont work until we add a bunch of Node->literal->val mapping

--- tests/FSharp.RDF.Tests/Tests.fs
+++ tests/FSharp.RDF.Tests/Tests.fs
@@ -57,15 +57,48 @@ let functionalProperties = """
        :pr3 "avalue"^^xsd:string
 """
 
+let someOfProperties = """
+@prefix : <http://testing.stuff/ns#> .
+@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+@base <http://testing.stuff/ns> .
+
+:item1 rdf:type :type1;
+       :pr1 :item2 .
+
+:item2 rdf:type :type2;
+       :pr2 :item3 .
+       :pr2 :item4 .
+       :pr2 :item5 .
+
+:item3 rdf:type :type3;
+       :pr3 "1"^^xsd:string
+:item3 rdf:type :type3;
+       :pr3 "2"^^xsd:string
+:item3 rdf:type :type3;
+       :pr3 "3"^^xsd:string
+"""
+let item1 = Uri.from "http://testing.stuff/ns#item1"
+let pr1 = Predicate.from "http://testing.stuff/ns#pr1"
+let pr2 = Predicate.from "http://testing.stuff/ns#pr2"
+let pr3 = Predicate.from "http://testing.stuff/ns#pr3"
+
 open Store
 
 [<Test>]
 let ``Traverse functional properties``() =
   let g = Graph.from functionalProperties
-  let s = Uri.from "http://testing.stuff/ns#item1"
-  let pr1 = Predicate.from "http://testing.stuff/ns#pr1"
-  let pr2 = Predicate.from "http://testing.stuff/ns#pr2"
-  let pr3 = Predicate.from "http://testing.stuff/ns#pr3"
-  let sx = fromSubject s g
+  let sx = fromSubject item1 g
   let avalue = [ sx ] ==> pr1 ==> pr2 .> pr3 <--*> string
-  Assert.AreEqual(avalue, "avalue")
+  Assert.AreEqual(avalue, ["avalue"])
+
+[<Test>]
+let ``Traverse nonfunctional properties``() =
+  let g = Graph.from functionalProperties
+  let sx = fromSubject item1 g
+  let avalue = [ sx ] ==> pr1 ==> pr2 .> pr3 <--*> string
+  Assert.AreEqual(avalue, ["1";"2";"3"])
+
+
+
